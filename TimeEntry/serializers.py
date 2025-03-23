ff4979ce7d82ef55afd31e4e6580c96859e6aaa7
Damian Ignaczak <vaqmad@icloud.com>
2025-03-23 16:37:13 +0100

    Add TimeEntry creation endpoint with serializer and view
    Introduced `TimeEntryCreateSerializer` for creating time entries with validation and ownership checks. Added `TimeEntryCreateView`. Temporary URL pattern added for testing purposes.

---
+++ TimeEntry/serializers.py
@@ -0,0 +1,36 @@
+# Django imports
+from django.shortcuts import get_object_or_404
+# DRF imports
+from rest_framework import serializers
+# Internal imports
+from .models import TimeEntry
+from Task.models import Task
+from TimeMate.Utils.mixins import OwnerRepresentationMixin
+from .validators import validate_start_and_end_time
+
+
+class TimeEntryCreateSerializer(OwnerRepresentationMixin, serializers.ModelSerializer):
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = TimeEntry
+        fields = ['owner', 'task', 'start_time', 'end_time', 'created_at']
+        read_only_fields = ['created_at']
+
+    def validate(self, data):
+        data = super().validate(data)
+
+        # Get necessary values
+        start_time = data.get('start_time')
+        end_time = data.get('end_time')
+        # Calling custom validator
+        validate_start_and_end_time(start_time, end_time)
+        return data
+
+    def validate_task(self, value):
+        # Ensures Task by given UUID exists
+        task = get_object_or_404(Task, pk=value.id)
+        # Check is user, owner of this task
+        if task.owner != self.context['request'].user:
+            raise serializers.ValidationError("You do not have permission to use this task.")
+        return value
--- TimeEntry/views.py
+++ TimeEntry/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# DRF imports
+from rest_framework import generics
+# Internal imports
+from .serializers import TimeEntryCreateSerializer
 
-# Create your views here.
+
+class TimeEntryCreateView(generics.CreateAPIView):
+    serializer_class = TimeEntryCreateSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        # Pass request and user to the serializer context
+        context["request"] = self.request
+        return context
