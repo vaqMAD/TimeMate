406cc2d2e602ee944e1bfed21f8449e2eb440883
Damian Ignaczak <vaqmad@icloud.com>
2025-03-24 17:09:35 +0100

    WIP: Refactor TimeEntry views. Consolidated separate create and list views into a single `TimeEntryListCreateView`.

--- TimeEntry/urls.py
+++ TimeEntry/urls.py
@@ -1,12 +1,12 @@
 # Django imports
 from django.urls import path, include
 # Internal imports
-from .views import TimeEntryCreateView
+from .views import TimeEntryListCreateView
 
 urlpatterns = [
     # TBA Tests purposes only
-    path('create/', TimeEntryCreateView.as_view(), name='time_entry_create'),
+    path('', TimeEntryListCreateView.as_view(), name='time_entry_list_create'),
 ]
 
 # TODO:
-# 1. Change URLs patterns.
+# 1. Change the list of the URL paths, so that it complies with REST standards
--- TimeEntry/views.py
+++ TimeEntry/views.py
@@ -1,14 +1,27 @@
 # DRF imports
 from rest_framework import generics
 # Internal imports
-from .serializers import TimeEntryCreateSerializer
+from .models import TimeEntry
+from .serializers import TimeEntryCreateSerializer, TimeEntryListSerializer
 
+# TODO [InProgress] : Check structure of the code, whether it meets Django standards
+# TODO [InProgress] : Add  unit and integration tests for both GET and POST views.
+# TODO [InProgress] : Think about whether structure of the GET response is appropriate.
+class TimeEntryListCreateView(generics.ListCreateAPIView):
 
-class TimeEntryCreateView(generics.CreateAPIView):
-    serializer_class = TimeEntryCreateSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return TimeEntryCreateSerializer
+        return TimeEntryListSerializer
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         # Pass request and user to the serializer context
         context["request"] = self.request
         return context
+
+    def get_queryset(self):
+        return TimeEntry.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
