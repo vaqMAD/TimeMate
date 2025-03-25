cfd2104fd4921748a79e3288c3603f1b57cb0d4f
Damian Ignaczak <vaqmad@icloud.com>
2025-03-25 07:00:33 +0100

    Ensure end_time is strictly greater than start_time.
    Previously, the validator only checked if end_time was less than start_time.

--- TimeEntry/validators.py
+++ TimeEntry/validators.py
@@ -5,5 +5,5 @@ def validate_start_and_end_time(start_time, end_time):
     """
     Check if end_time is greater than start_time
     """
-    if end_time < start_time:
+    if end_time < start_time or end_time == start_time:
         raise ValidationError(f"End time {end_time}, must be greater than start time {start_time}")
