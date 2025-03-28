2f4e69761e632987941a1fa8851f5472bccd7afc
Damian Ignaczak <vaqmad@icloud.com>
2025-03-28 13:59:32 +0100

    Update pagination to use DefaultPagination in views
    Replaced specific TaskPagination with DefaultPagination in `TaskListView` and added DefaultPagination to `TimeEntryListCreateView`.

--- TimeEntry/views.py
+++ TimeEntry/views.py
@@ -3,12 +3,14 @@ from rest_framework import generics
 # Internal imports
 from .models import TimeEntry
 from .serializers import TimeEntryCreateSerializer, TimeEntryListSerializer
+from TimeMate.Utils.pagination import DefaultPagination
 
 
 # TODO [InProgress] : Check structure of the code, whether it meets Django standards
 # TODO [InProgress] : Add  unit and integration tests for both GET and POST views.
 # TODO [InProgress] : Think about whether structure of the GET response is appropriate.
 class TimeEntryListCreateView(generics.ListCreateAPIView):
+    pagination_class = DefaultPagination
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
