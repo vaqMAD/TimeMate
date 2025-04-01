3ca2dd77124af2b5f5200a940215389fc8b5ff9b
Damian Ignaczak <vaqmad@icloud.com>
2025-04-01 16:25:52 +0200

    Add TimeEntryDetailView for detailed entry operations
    Introduced TimeEntryDetailView to handle retrieve, update, and delete operations for TimeEntry objects.

--- TimeEntry/views.py
+++ TimeEntry/views.py
@@ -2,8 +2,9 @@
 from rest_framework import generics
 # Internal imports
 from .models import TimeEntry
-from .serializers import TimeEntryCreateSerializer, TimeEntryListSerializer
+from .serializers import TimeEntryCreateSerializer, TimeEntryListSerializer, TimeEntryDetailSerializer
 from TimeMate.Utils.pagination import DefaultPagination
+from TimeMate.Permissions.owner_permissions import IsObjectOwner
 
 
 # TODO [InProgress] : Check structure of the code, whether it meets Django standards
@@ -19,3 +20,13 @@ class TimeEntryListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return TimeEntry.objects.filter(owner=self.request.user)
+
+# # TODO [InProgress/NOTE] : This test class is a Work In Progress.
+# The existing tests are under active development and are subject to further reviews and refinements.
+class TimeEntryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsObjectOwner]
+    serializer_class = TimeEntryDetailSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        return TimeEntry.objects.filter(pk=pk)
