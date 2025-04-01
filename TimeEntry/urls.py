94418917bf17d892e03dfbd1d2279afaa5218dc9
Damian Ignaczak <vaqmad@icloud.com>
2025-04-01 16:23:48 +0200

    Add detail view URL for TimeEntry
    Introduced a new URL for the TimeEntryDetailView, allowing access to individual TimeEntry instances via a UUID.

--- TimeEntry/urls.py
+++ TimeEntry/urls.py
@@ -1,11 +1,12 @@
 # Django imports
 from django.urls import path, include
 # Internal imports
-from .views import TimeEntryListCreateView
+from .views import TimeEntryListCreateView, TimeEntryDetailView
 
 urlpatterns = [
     # TBA Tests purposes only
     path('', TimeEntryListCreateView.as_view(), name='time_entry_list_create'),
+    path('<uuid:pk>/', TimeEntryDetailView.as_view(), name='time_entry_detail'),
 ]
 
 # TODO:
