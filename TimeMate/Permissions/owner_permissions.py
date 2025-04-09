6c595333820e13c9a803ff123f09cef883ebcae0
Damian Ignaczak <vaqmad@icloud.com>
2025-04-09 16:38:40 +0200

    Refactor permission handling to raise detailed exceptions
    Updated `IsObjectOwner` to raise a `PermissionDenied` exception with a specific error code when access is denied.

--- TimeMate/Permissions/owner_permissions.py
+++ TimeMate/Permissions/owner_permissions.py
@@ -1,10 +1,15 @@
+# Permissions error codes
+PERMISSION_ERROR_CODE_NOT_TASK_OWNER = "not_task_owner"
 # DRF Imports
 from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import PermissionDenied
 
 
 class IsObjectOwner(BasePermission):
-    """
-    Custom permission that allows access only to object owners.
-    """
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        if obj.owner != request.user:
+            raise PermissionDenied(
+                detail="You do not have permission to access this task",
+                code=PERMISSION_ERROR_CODE_NOT_TASK_OWNER
+            )
+        return True
