7f250a4ca6f39033a20c01c8ccf06f6512376a41
Damian Ignaczak <vaqmad@icloud.com>
2025-04-23 19:20:17 +0200

    Add views to display sorted `TimeEntry` objects against `Task` objects - `TimeEntrySortedByTaskNameListView` view.

--- TimeEntry/views.py
+++ TimeEntry/views.py
@@ -1,24 +1,28 @@
 # Django imports
+from django.db.models import (
+    F,
+    ExpressionWrapper,
+    DurationField, Prefetch
+)
 from django_filters.rest_framework import DjangoFilterBackend
 # DRF imports
 from rest_framework.filters import OrderingFilter
 from rest_framework import generics
 # Internal imports
+from Task.models import Task
 from .models import TimeEntry
 from .serializers import (
     TimeEntryCreateSerializer,
     TimeEntryListSerializer,
     TimeEntryDetailSerializer,
-    TimeEntryUpdateSerializer
+    TimeEntryUpdateSerializer,
+    TaskWithTimeEntriesSerializer
 )
 from TimeMate.Utils.pagination import DefaultPagination
 from .filters import TimeEntryFilter
 from TimeMate.Permissions.owner_permissions import IsObjectOwner
 
 
-# TODO [InProgress] : Check structure of the code, whether it meets Django standards
-# TODO [InProgress] : Add  unit and integration tests for both GET and POST views.
-# TODO [InProgress] : Think about whether structure of the GET response is appropriate.
 class TimeEntryListCreateView(generics.ListCreateAPIView):
     pagination_class = DefaultPagination
     filter_backends = [DjangoFilterBackend, OrderingFilter]
@@ -31,11 +35,9 @@ class TimeEntryListCreateView(generics.ListCreateAPIView):
         return TimeEntryListSerializer
 
     def get_queryset(self):
-        return TimeEntry.objects.filter(owner=self.request.user)
+        return TimeEntry.objects.filter(owner=self.request.user).select_related('task', 'owner')
 
 
-# # TODO [InProgress/NOTE] : This test class is a Work In Progress.
-# The existing tests are under active development and are subject to further reviews and refinements.
 class TimeEntryDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsObjectOwner]
 
@@ -46,4 +48,25 @@ class TimeEntryDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         pk = self.kwargs.get("pk")
-        return TimeEntry.objects.filter(pk=pk)
+        return TimeEntry.objects.filter(pk=pk).select_related('task', 'owner')
+
+class TimeEntrySortedByTaskNameListView(generics.ListAPIView):
+    serializer_class = TaskWithTimeEntriesSerializer
+    pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = TimeEntryFilter
+    ordering_fields = ['start_time', 'end_time', 'task__name', 'entries__duration']
+
+    def get_queryset(self):
+        task_qs = Task.objects.filter(owner=self.request.user)
+
+        time_entries_qs = TimeEntry.objects.annotate(
+            duration=ExpressionWrapper(
+                F('end_time') - F('start_time'),
+                output_field=DurationField()
+            )
+        ).order_by('task__name')
+
+        return task_qs.prefetch_related(
+            Prefetch('time_entries',queryset=time_entries_qs )
+        )
