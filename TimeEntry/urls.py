9107b8a24e745ce2bfe9a12d1df6415a4f8b391b
Damian Ignaczak <vaqmad@icloud.com>
2025-04-24 16:28:43 +0200

    Add basic url paths for `TimeEntrySortedByTaskNameListView` view.

--- TimeEntry/urls.py
+++ TimeEntry/urls.py
@@ -1,13 +1,15 @@
 # Django imports
 from django.urls import path, include
 # Internal imports
-from .views import TimeEntryListCreateView, TimeEntryDetailView
+from .views import (
+    TimeEntryListCreateView,
+    TimeEntryDetailView,
+    TimeEntrySortedByTaskNameListView
+)
 
 urlpatterns = [
     # TBA Tests purposes only
     path('', TimeEntryListCreateView.as_view(), name='time_entry_list_create'),
     path('<uuid:pk>/', TimeEntryDetailView.as_view(), name='time_entry_detail'),
+    path('sorted-by-task-name/', TimeEntrySortedByTaskNameListView.as_view(), name='time_entry_sorted_by_task_name'),
 ]
-
-# TODO:
-# 1. Change the list of the URL paths, so that it complies with REST standards
