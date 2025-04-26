469bcd38f73402a97011d6c4b4534892049b0247
Damian Ignaczak <vaqmad@icloud.com>
2025-04-26 14:50:16 +0200

    Add API schema and documentation endpoints
    Integrated Spectacular API views to provide schema generation and documentation.

--- TimeMate/urls.py
+++ TimeMate/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('tasks/', include('Task.urls')),
-                  path('time-entries/', include('TimeEntry.urls')),
-                  path('api-auth/', include('rest_framework.urls')),
-              ]
+    path('admin/', admin.site.urls),
+    path('tasks/', include('Task.urls')),
+    path('time-entries/', include('TimeEntry.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
 urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
