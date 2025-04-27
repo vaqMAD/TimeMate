3d0127160b590294c15e2a067d8e4e490dc01810
Damian Ignaczak <vaqmad@icloud.com>
2025-04-27 07:42:18 +0200

    Refactor TimeEntry serializers to improve reuse and clarity
    Introduced a `TimeEntryBaseSerializer` to centralize shared fields and logic across serializers. Updated other serializers to inherit from the base serializer, simplifying and reducing redundant code. Removed `TimeEntryNestedSerializerWithDurationField`, consolidating its functionality into the base serializer.

--- TimeEntry/serializers.py
+++ TimeEntry/serializers.py
@@ -1,5 +1,3 @@
-# Django imports
-from django.shortcuts import get_object_or_404
 # DRF imports
 from rest_framework import serializers
 # Internal imports
@@ -10,14 +8,18 @@ from TimeMate.Utils.mixins import OwnerRepresentationMixin
 from .validators import validate_start_and_end_time
 from Task.validators import validate_task_ownership
 
+class TimeEntryBaseSerializer(serializers.ModelSerializer):
+    detail_url = serializers.HyperlinkedIdentityField(read_only=True, view_name='time_entry_detail')
+    class Meta:
+        model = TimeEntry
+        fields = ['id', 'start_time', 'end_time', 'duration',  'detail_url']
 
 class TimeEntryCreateSerializer(OwnerRepresentationMixin, serializers.ModelSerializer):
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
         model = TimeEntry
-        fields = ['owner', 'task', 'start_time', 'end_time', 'created_at']
-        read_only_fields = ['created_at']
+        fields = ['owner', 'task', 'start_time', 'end_time']
 
     def validate(self, data):
         data = super().validate(data)
@@ -34,21 +36,18 @@ class TimeEntryCreateSerializer(OwnerRepresentationMixin, serializers.ModelSeria
         return validate_task_ownership(value, self.context['request'].user)
 
 
-class TimeEntryListSerializer(serializers.ModelSerializer):
+class TimeEntryListSerializer(TimeEntryBaseSerializer):
     task = TaskListSerializer(read_only=True)
-    detail_url = serializers.HyperlinkedIdentityField(read_only=True, view_name='time_entry_detail')
 
-    class Meta:
-        model = TimeEntry
-        fields = ['id', 'task', 'start_time', 'end_time', 'detail_url']
+    class Meta(TimeEntryBaseSerializer.Meta):
+        fields = ['id', 'task', 'start_time', 'end_time', 'duration', 'detail_url']
 
 
 class TimeEntryDetailSerializer(OwnerRepresentationMixin, serializers.ModelSerializer):
     task = TaskListSerializer(read_only=True)
 
-    class Meta:
-        model = TimeEntry
-        fields = ['id', 'task', 'start_time', 'end_time', 'owner', 'created_at']
+    class Meta(TimeEntryBaseSerializer.Meta):
+        fields = ['id', 'task', 'start_time', 'end_time', 'duration', 'owner', 'created_at']
 
 
 class TimeEntryUpdateSerializer(serializers.ModelSerializer):
@@ -56,7 +55,7 @@ class TimeEntryUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TimeEntry
-        fields = ['id', 'task', 'start_time', 'end_time', 'owner']
+        fields = ['id', 'task', 'start_time', 'end_time', 'duration', 'owner']
 
     def validate(self, data):
         data = super().validate(data)
@@ -71,27 +70,8 @@ class TimeEntryUpdateSerializer(serializers.ModelSerializer):
     def validate_task(self, value):
         return validate_task_ownership(value, self.context['request'].user)
 
-
-class TimeEntryNestedSerializerWithDurationField(serializers.ModelSerializer):
-    duration = serializers.SerializerMethodField()
-    detail_url = serializers.HyperlinkedIdentityField(
-        read_only=True,
-        view_name='time_entry_detail'
-    )
-
-    class Meta:
-        model = TimeEntry
-        fields = ['id', 'start_time', 'end_time', 'duration', 'detail_url']
-
-    def get_duration(self, obj):
-        # Queryset should have 'duration' annotation
-        if hasattr(obj, 'duration'):
-            return obj.duration
-        return obj.end_time - obj.start_time
-
-
 class TaskWithTimeEntriesSerializer(serializers.ModelSerializer):
-    entries = TimeEntryNestedSerializerWithDurationField(
+    entries = TimeEntryBaseSerializer(
         source='time_entries',
         many=True,
         read_only=True
