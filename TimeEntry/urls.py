21d619efbc5ee599e9308be5d27eb8642517ee5a
Damian Ignaczak <vaqmad@icloud.com>
2025-04-27 07:45:05 +0200

    Refactor imports after renaming views.

--- TimeEntry/urls.py
+++ TimeEntry/urls.py
@@ -4,12 +4,12 @@ from django.urls import path, include
 from .views import (
     TimeEntryListCreateView,
     TimeEntryDetailView,
-    TimeEntrySortedByTaskNameListView
+    TimeEntriesByTaskListView,
 )
 
 urlpatterns = [
     # TBA Tests purposes only
     path('', TimeEntryListCreateView.as_view(), name='time_entry_list_create'),
     path('<uuid:pk>/', TimeEntryDetailView.as_view(), name='time_entry_detail'),
-    path('sorted-by-task-name/', TimeEntrySortedByTaskNameListView.as_view(), name='time_entry_sorted_by_task_name'),
+    path('sorted-by-task-name/', TimeEntriesByTaskListView.as_view(), name='time_entry_sorted_by_task_name'),
 ]
