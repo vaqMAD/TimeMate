23a32fba08947995c0a4c7f75a22153ad887c9c7
Damian Ignaczak <vaqmad@icloud.com>
2025-05-01 18:39:43 +0200

    Initialize signal imports in app configurations
    Added the `ready` method to both `TaskConfig` and `TimeentryConfig` to import signals. This ensures the signals are correctly loaded when the respective apps are initialized.

--- TimeEntry/apps.py
+++ TimeEntry/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TimeentryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'TimeEntry'
+
+    def ready(self):
+        import TimeMate.Signals.signals
