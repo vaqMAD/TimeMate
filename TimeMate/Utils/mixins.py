5b46c6228bb559780e1253adc029448479144aaa
Damian Ignaczak <vaqmad@icloud.com>
2025-05-01 18:38:32 +0200

    Add CacheListMixin to enable caching for list views
    Introduced CacheListMixin to cache responses for list views, improving performance and reducing database queries. The mixin generates unique cache keys based on user and query parameters, with a default timeout of 300 seconds.

--- TimeMate/Utils/mixins.py
+++ TimeMate/Utils/mixins.py
@@ -1,3 +1,8 @@
+# Django imports
+from django.core.cache import cache
+# DRF imports
+from rest_framework.response import Response
+# Internal imports
 from TimeMate.Serializers.user_serializers import UserSerializer
 
 class OwnerRepresentationMixin:
@@ -8,3 +13,20 @@ class OwnerRepresentationMixin:
             # Return owner field as a nested serialized object, not as an ID
             representation['owner'] = UserSerializer(instance.owner).data
         return representation
+
+
+class CacheListMixin:
+    cache_timeout = 300
+
+    def list(self, request, *args, **kwargs):
+        key = self.get_cache_key(request)
+        cached_data = cache.get(key)
+        if cached_data is not None:
+            return Response(cached_data)
+        response = super().list(request, *args, **kwargs)
+        cache.set(key, response.data, self.cache_timeout)
+        return response
+
+    def get_cache_key(self, request):
+        params = request.query_params.urlencode()
+        return f'{self.__class__.__name__}:user={request.user.id}:{params}'
